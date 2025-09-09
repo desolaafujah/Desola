509abf10511ebc506162808808788b8988e3c125
Desola Fujah <desolafujah@Desolas-MacBook-Air.local>
2025-09-09 00:25:25 -0400

    added darker brown background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { ArrowDown, Mail } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import heroBackground from "@/assets/hero-background.jpg";
+import heroBackground from "@/assets/calmbrown.png";
 
 const Hero = () => {
   const scrollToNext = () => {
